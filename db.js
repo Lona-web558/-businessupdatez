222bf650df8eb8cc224be482ca1e011a079342d2
Lona Smith <71089848+Lona-web558@users.noreply.github.com>
2026-07-23 21:22:03 +0200

    Update db.js

--- db.js
+++ db.js
@@ -5,7 +5,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const DATA_FILE = path.join(__dirname, "articles.json", "articles1.json");
+const DATA_FILE = path.join(__dirname, "articles.json");
 
 function readArticles() {
   try {
