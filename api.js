1d896896e3faf2d7fd3f6b25b7f2a0912fc3424d
Lona Smith <71089848+Lona-web558@users.noreply.github.com>
2026-07-23 21:20:37 +0200

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("./db1");
+const db = require("./db");
 const market = require("./market");
 
 // Simple shared-secret admin check.
